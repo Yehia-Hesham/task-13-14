043fec2ae204b99468f4a005f1533cc96655dbfd
Yehia Hesham <Yehia.Hesham@itworx.com>
2022-07-21 10:37:11 +0200

    final touches

--- task 15 Placesv2/script/scripts.js
+++ task 15 Placesv2/script/scripts.js
@@ -4,6 +4,7 @@
 $("div.card").click(function(){
     var selector = $(this).attr('data-place');
     $(".content").removeClass("is-active");
+    console.log(selector);
     // $("nav-item").removeClass("is-active");
     $(".content[data-place="+selector+"]").addClass("is-active");
 });
